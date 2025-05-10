f9a3f27e3b05cf4c9d2b2967655060b1644ead14
Akhilesh201988 <akisingh201988@gmail.com>
2025-05-10 15:28:54 +0530

    10th May changes

---
+++ src/Ex_01_Java_basics/Testcomment.java
@@ -0,0 +1,21 @@
+package Ex_01_Java_basics;
+
+public class Testcomment {
+    public static void main(String[] args) {
+        System.out.println("Test comment");
+        System.out.println("Test comment");
+
+        //Print will print on same line
+        System.out.print("Test comment");
+        System.out.print("Test comment");
+
+
+        //Control D will copy the Sout statements
+        // Prtintln will print on next line
+
+        // this test comment
+        /** this is test comment
+         Written by Akhilesh
+         /
+    }
+}
--- src/Ex_01_Java_basics/Testpush.java
+++ src/Ex_01_Java_basics/Testpush.java
@@ -2,6 +2,6 @@ package Ex_01_Java_basics;
 
 public class Testpush {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test  ");
     }
 }
