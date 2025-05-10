452af1858fc29947ac97350ff247c8dd1952a5f3
Akhilesh201988 <akisingh201988@gmail.com>
2025-05-10 15:37:46 +0530

    10th May changes

---
+++ src/Ex_01_Java_basics/JavaProgramInteger.java
@@ -0,0 +1,15 @@
+package Ex_01_Java_basics;
+
+public class JavaProgramInteger {
+    public static void main(String[] args) {
+        System.out.println(2+2);
+        System.out.println(2-2);
+        System.out.println(2/2);
+        System.out.println(2*2);
+        System.out.println(3/2);
+        // System.out.println(3/2) println ->Integer part -> No decimal
+
+
+
+    }
+}
--- src/Ex_01_Java_basics/Testcomment.java
+++ src/Ex_01_Java_basics/Testcomment.java
@@ -10,12 +10,5 @@ public class Testcomment {
         System.out.print("Test comment");
 
 
-        //Control D will copy the Sout statements
-        // Prtintln will print on next line
-
-        // this test comment
-        /** this is test comment
-         Written by Akhilesh
-         /
     }
 }
