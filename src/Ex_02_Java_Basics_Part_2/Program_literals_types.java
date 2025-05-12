e298b915fb8288b9af8dd1cd9ae0c3039375014b
Akhilesh201988 <akisingh201988@gmail.com>
2025-05-13 00:19:47 +0530

    11th May changes

---
+++ src/Ex_02_Java_Basics_Part_2/Program_literals_types.java
@@ -0,0 +1,25 @@
+package Ex_02_Java_Basics_Part_2;
+
+public class Program_literals_types {
+    public static void main(String[] args) {
+
+        /**
+         * ### **Types of Literals**
+         * 1. Boolean Literals - true, false
+         * 2. Character Literals
+         * 3. String Literals
+         * 4. Integral Literals
+         * 5. Floating Literals
+         * 6. null Literal , **Scientific** Literals
+         */
+        int i=65;
+        // Literals= Integral
+
+       final float f=3.14f;
+        // Literals= float
+
+        final int girl_age=18;
+        // Literals= final Integral
+
+    }
+}
---
+++ src/Ex_03_Literals_Part_3/Types_of_Literals.java
@@ -0,0 +1,27 @@
+package Ex_03_Literals_Part_3;
+
+public class Types_of_Literals {
+
+        public static void main(String[] args) {
+
+            /**
+             * ### **Types of Literals**
+             * 1. Boolean Literals - true, false
+             * 2. Character Literals
+             * 3. String Literals
+             * 4. Integral Literals
+             * 5. Floating Literals
+             * 6. null Literal , **Scientific** Literals
+             */
+            int i=65;
+            // Literals= Integral
+
+            final float f=3.14f;
+            // Literals= float
+
+            final int girl_age=18;
+            // Literals= final Integral
+
+        }
+    }
+
